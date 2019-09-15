a66d0cc0fb61e3a93fed7a429b3baa3d246bc858
Dallin Moak <dallinmoak@gmail.com>
2019-09-15 15:41:22 -0600

    add scanner abstract hello functions

--- DallinTest.java
+++ DallinTest.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class DallinTest{
   public static void main (String args[]){
-    System.out.println("Hello World!");
+    sayHello();
+  }
+
+  private static void sayHello() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("What's your name?");
+    String name = input.next();
+    String hey = "Hello, " + name+ "!";
+    System.out.println(hey);
   }
 }
